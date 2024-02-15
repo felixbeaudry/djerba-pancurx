6b19057d4eec84f668e7f52c8ff7dbac382bb2ba
Felix Beaudry <fbeaudry@oicr.on.ca>
2024-02-15 13:03:59 -0500

    update

--- src/lib/djerba/plugins/pancurx/patient_info/test/plugin_test.py
+++ src/lib/djerba/plugins/pancurx/patient_info/test/plugin_test.py
@@ -22,24 +22,6 @@ class TestPatientInfo(PluginTester):
         }
         self.run_basic_test(test_source_dir, params)
 
-    def test_redact(self):
-        # test the redact method
-        test_source_dir = os.path.realpath(os.path.dirname(__file__))
-        kwargs = {
-            cc.IDENTIFIER: 'patient_info',
-            cc.MODULE_DIR: os.path.realpath(os.path.join(test_source_dir, '..')),
-            cc.LOG_LEVEL: logging.WARNING,
-            cc.LOG_PATH: None,
-            cc.WORKSPACE: workspace(self.tmp_dir)
-        }
-        plugin = patient_info_plugin(**kwargs)
-        with open(os.path.join(test_source_dir, 'patient_info.json')) as in_file:
-            data = json.loads(in_file.read())
-        redacted = plugin.redact(data)
-        with open(os.path.join(test_source_dir, 'patient_info_redacted.json')) as in_file:
-            expected = json.loads(in_file.read())
-        self.assertEqual(redacted, expected)
-
 if __name__ == '__main__':
     unittest.main()
 
