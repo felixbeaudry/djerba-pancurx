16753fb97089b7300500c797701fc2272d97eac9
Felix Beaudry <fbeaudry@oicr.on.ca>
2024-02-15 13:14:38 -0500

    update

--- src/lib/djerba/core/html/sorttable.js
+++ src/lib/djerba/core/html/sorttable.js
@@ -2,15 +2,7 @@
   SortTable
   version 2
   7th April 2007
-  Stuart Langridge, http://www.kryogenix.org/code/browser/sorttable/
-  
-  Instructions:
-  Download this file
-  Add <script src="sorttable.js"></script> to your HTML
-  Add class="sortable" to any table you'd like to make sortable
-  Click on the headers to sort
-  
-  Thanks to many, many people for contributions and suggestions.
+  Stuart Langridge, http://www.kryogenix.org/code/browser/sorttable/  
   Licenced as X11: http://www.kryogenix.org/code/browser/licence.html
   This basically means: do what you want with it.
 */
--- src/lib/djerba/plugins/pancurx/patient_info/test/plugin_test.py
+++ src/lib/djerba/plugins/pancurx/patient_info/test/plugin_test.py
@@ -18,7 +18,7 @@ class TestPatientInfo(PluginTester):
         params = {
             self.INI: 'patient_info.ini',
             self.JSON: 'patient_info.json',
-            self.MD5: 'd6c72c8d6f8d8246a4bd25a0dd0d191f'
+            self.MD5: 'd41ee59fa3b464a665e8517fe04c12b9'
         }
         self.run_basic_test(test_source_dir, params)
 
